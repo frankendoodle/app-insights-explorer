306df01d2ab06497715597aef2d437b4c080b404
Frank Goulart <fggoulart@yahoo.com>
2026-04-22 16:38:13 -0700

    PLATFORM-193: add infra/envs/staging/backend.tf

---
+++ infra/envs/staging/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "azurerm" {
+    container_name = "tfstate"
+    key            = "app-insights-explorer/staging.tfstate"
+    # resource_group_name and storage_account_name are passed via -backend-config at runtime
+  }
+}
