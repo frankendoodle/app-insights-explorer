4612ebc572c854d41828c8b5ada21e29fe70c721
Frank Goulart <fggoulart@yahoo.com>
2026-05-05 10:56:56 -0700

    PLATFORM-3: add API web app to infra/envs/test/

--- infra/envs/test/webapps.tf
+++ infra/envs/test/webapps.tf
@@ -27,3 +27,33 @@ resource "azurerm_linux_web_app" "frontend" {
     ]
   }
 }
+
+resource "azurerm_linux_web_app" "api" {
+  name                = "app-aie-api-test-tfg"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  service_plan_id     = azurerm_service_plan.test.id
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  site_config {
+    always_on = false
+
+    application_stack {
+      docker_image_name   = "app-insights-explorer-api:latest"
+      docker_registry_url = "https://${var.acr_login_server}"
+    }
+  }
+
+  app_settings = {
+    # Settings added in Task 11
+  }
+
+  lifecycle {
+    ignore_changes = [
+      app_settings,
+    ]
+  }
+}
